5e787013c851a1da338996ddf1f354a3f8e6150c
Wouter Roos <a-wouter.roos@daftrucks.com>
2017-10-20 14:13:45 +0200

    initial commit

---
+++ src/Qnnect.Case.AzureFunction/FaceApiDataEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json.Linq;
+
+namespace Qnnect.Case.AzureFunction
+{
+    public class FaceApiDataEntity : TableEntity
+    {
+        public Guid PhotoId { get; set; }
+
+        public string AnalysisResult { get; set; }
+
+        public FaceApiDataEntity(Guid photoId, JObject analysisResult)
+        {
+            PartitionKey = "Qnnect";
+            RowKey = photoId.ToString();
+            PhotoId = photoId;
+            AnalysisResult = analysisResult.ToString();
+        }
+    }
+}
---
+++ src/Qnnect.Case.UploadClient/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows;
+
+namespace Qnnect.Case.UploadClient
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpg)|*.png;*.jpg"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+            }
+        }
+    }
+}
